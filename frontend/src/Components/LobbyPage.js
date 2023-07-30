0d7aa4c6204808d48487a2ac08a44c765b5257d3
Mudra Verma <md393822@dal.ca>
2023-07-30 15:17:33 -0300

    Lobby page

--- frontend/src/Components/LobbyPage.js
+++ frontend/src/Components/LobbyPage.js
@@ -5,50 +5,61 @@ import GameDetails from './GameDetails';
 const LobbyPage = () => {
   const [category, setCategory] = useState('');
   const [difficulty, setDifficulty] = useState('');
-  const [timeRemaining, setTimeRemaining] = useState('');
+  const [duration, setDuration] = useState('');
   const [filteredGames, setFilteredGames] = useState([]);
-
-  const allGames = [
-    { id: 1, title: 'Game 1', category: 'Sports', difficulty: 'Medium', timeRemaining: 3600, participants: 2, Description: 'This is Game 1' },
-    { id: 2, title: 'Game 2', category: 'Computer Science', difficulty: 'Difficult', timeRemaining: 7200, participants: 3, Description: 'This is Game 2' },
-    { id: 3, title: 'Game 3', category: 'Nature', difficulty: 'Easy', timeRemaining: 1800, participants: 2, Description: 'This is Game 3' },
-    // Add more games here
-  ];
 
   useEffect(() => {
     // Filter games based on the selected criteria
-    let filteredGames = allGames;
-
-    // if (category) {
-    //   filteredGames = filteredGames.filter((game) => game.category === category);
-    // }
+    let filteredGames = [];
 
     if (category) {
       axios
         .post('https://gceak8vzy1.execute-api.us-east-1.amazonaws.com/dev/filteredgames', {
-          categoryName: category,
+          categoryId: category,
         })
         .then((response) => {
           console.log('POST request successful:', response.data);
           // Update the filteredGames state with the data received from the API
-          setFilteredGames(response.data);
+          setFilteredGames(response.data.response);
+          setCategory('');
         })
         .catch((error) => {
           console.error('POST request failed:', error);
-        });
+        }); 
     }
 
     if (difficulty) {
-      filteredGames = filteredGames.filter((game) => game.difficulty === difficulty);
+      axios
+        .post('https://gceak8vzy1.execute-api.us-east-1.amazonaws.com/dev/filteredgames', {
+          difficulty: difficulty,
+        })
+        .then((response) => {
+          console.log('POST request successful:', response.data);
+          // Update the filteredGames state with the data received from the API
+          setFilteredGames(response.data.response);
+          setDifficulty('');
+        })
+        .catch((error) => {
+          console.error('POST request failed:', error);
+        }); 
     }
 
-    if (timeRemaining) {
-      const timeRemainingInSeconds = timeRemaining === '30 minutes' ? 1800 : timeRemaining === '1 hour' ? 3600 : 7200;
-      filteredGames = filteredGames.filter((game) => game.timeRemaining <= timeRemainingInSeconds);
+    if (duration) {
+      axios
+        .post('https://gceak8vzy1.execute-api.us-east-1.amazonaws.com/dev/filteredgames', {
+          duration: duration,
+        })
+        .then((response) => {
+          console.log('POST request successful:', response.data);
+          // Update the filteredGames state with the data received from the API
+          setFilteredGames(response.data.response);
+          setDuration('');
+        })
+        .catch((error) => {
+          console.error('POST request failed:', error);
+        }); 
     }
-
-    setFilteredGames(filteredGames);
-  }, [category, difficulty, timeRemaining]);
+  }, [category, difficulty, duration]);
 
   const handleCategoryChange = (event) => {
     setCategory(event.target.value);
@@ -58,8 +69,11 @@ const LobbyPage = () => {
     setDifficulty(event.target.value);
   };
 
-  const handleTimeRemainingChange = (event) => {
-    setTimeRemaining(event.target.value);
+  const handleDurationChange = (event) => {
+    const selectedDuration = event.target.value;
+    const durationInt = parseInt(selectedDuration, 10);
+
+    setDuration(durationInt);
   };
 
   return (
@@ -70,9 +84,12 @@ const LobbyPage = () => {
             <label>Category:</label>
             <select className="form-control" value={category} onChange={handleCategoryChange}>
               <option value="">Select a category</option>
-              <option value="Sports">Sports</option>
-              <option value="Computer Science">Computer Science</option>
-              <option value="Nature">Nature</option>
+              <option value="DQF9lQHFq4N8zXS6CMpt">Cloud</option>
+              <option value="Euw0SWCIL97yPB11ok7C">Fitness</option>
+              <option value="TQzoGjwRcJkZPQyQ02bL">Sports</option>
+              <option value="rw4PQ5YiX3HeeDe2o38M">Computer Science</option>
+              <option value="tSfIYc21C2iuTt5Xtptv">Dance</option>
+              <option value="zqctudS2FktOjZdGuS6T">Music</option>
             </select>
           </div>
         </div>
@@ -81,20 +98,20 @@ const LobbyPage = () => {
             <label>Difficulty:</label>
             <select className="form-control" value={difficulty} onChange={handleDifficultyChange}>
               <option value="">Select a difficulty</option>
-              <option value="Easy">Easy</option>
-              <option value="Medium">Medium</option>
-              <option value="Difficult">Difficult</option>
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="difficult">Difficult</option>
             </select>
           </div>
         </div>
         <div className="col">
           <div className="form-group">
-            <label>Time Remaining:</label>
-            <select className="form-control" value={timeRemaining} onChange={handleTimeRemainingChange}>
+            <label>Duration:</label>
+            <select className="form-control" value={duration} onChange={handleDurationChange}>
               <option value="">Select time remaining</option>
-              <option value="30 minutes">30 minutes</option>
-              <option value="1 hour">1 hour</option>
-              <option value="2 hours">2 hours</option>
+              <option value="40">40 minutes</option>
+              <option value="80">80 minutes</option>
+              <option value="120">2 hours</option>
             </select>
           </div>
         </div>
