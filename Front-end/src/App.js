daed9c5e3cdb2c30da6795ed9c24d7af7a705965
Mudra Verma <md393822@dal.ca>
2023-08-02 03:04:35 -0300

    User Team Management

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -10,6 +10,8 @@ import Leaderboard from './Pages/Leaderboard';
 import UserProfile from './Pages/Profilepage';
 import JoinGame from './Pages/Joingame';
 import Quiz from './Pages/Quiz';
+import TeamDetails from './Pages/TeamDetails';
+import CreateTeam from './Pages/CreateTeam';
 function App() {
 
   useEffect(() => {
@@ -40,6 +42,8 @@ function App() {
         <Route path='/UserProfile' element={<UserProfile /> } />
         <Route path='/joingame' element={<JoinGame /> } />
         <Route path='/quiz' element={<Quiz />} />
+        <Route path='/teams' element={<TeamDetails />} />
+        <Route path='/create-team' element={<CreateTeam />} />
       </Routes>
     </BrowserRouter>
   );
