d9fc04b6957faa6038c91dfdcb5532a9ca327973
Mudra Verma <md393822@dal.ca>
2023-08-05 01:45:57 -0300

    Refactoring

--- Front-end/src/Pages/TeamStats.js
+++ Front-end/src/Pages/TeamStats.js
@@ -5,15 +5,20 @@ import Header from '../Components/Header';
 
 const TeamStats = () => {
   const [teamDetails, setTeamDetails] = useState([]);
+  const [joinOrCreateMessage, setJoinOrCreateMessage] = useState('');
 
   useEffect(() => {
     // Fetch team details from the API
     const fetchTeamDetails = async () => {
       try {
         const teamName = localStorage.getItem('teamName');
-        const response = await axios.get(`https://wlfhjj5a5a.execute-api.us-east-1.amazonaws.com/game/team-stats?TeamName=${teamName.replace(/\s/g, '_')}`);
-        setTeamDetails(response.data.body);
-        console.log(response.data.body)
+        if (!teamName) {
+          setJoinOrCreateMessage('Create or Join a Team');
+        } else {
+          const response = await axios.get(`https://wlfhjj5a5a.execute-api.us-east-1.amazonaws.com/game/team-stats?TeamName=${teamName.replace(/\s/g, '_')}`);
+          setTeamDetails(response.data.body);
+          console.log(response.data.body);
+        }
       } catch (error) {
         console.error(error);
       }
@@ -24,47 +29,50 @@ const TeamStats = () => {
 
   return (
     <div>
-        <Header/>
-    
-    <div className="container mt-4">
-      {teamDetails ? (
-        <>
-          <h2 className="text-center mb-4">{teamDetails.TeamName}</h2>
-          <div className="card mb-4">
-            <div className="card-header">
-              <h4>Members</h4>
+      <Header />
+
+      <div className="container mt-4">
+        {joinOrCreateMessage ? (
+          <p className="text-center">{joinOrCreateMessage}</p>
+        ) : teamDetails && Object.keys(teamDetails).length > 0 ? (
+          <>
+            <h2 className="text-center mb-4">{teamDetails.TeamName}</h2>
+            <div className="card mb-4">
+              <div className="card-header">
+                <h4>Members</h4>
+              </div>
+              <div className="card-body">
+                <ul className="list-group">
+                  {teamDetails.Members &&
+                    teamDetails.Members.map((member) => (
+                      <li key={member} className="list-group-item">
+                        {member}
+                      </li>
+                    ))}
+                </ul>
+              </div>
             </div>
-            <div className="card-body">
-              <ul className="list-group">
-                {teamDetails.Members.map((member) => (
-                  <li key={member} className="list-group-item">
-                    {member}
-                  </li>
-                ))}
-              </ul>
+            <div className="card mb-4">
+              <div className="card-header">
+                <h4>Games Played</h4>
+              </div>
+              <div className="card-body">
+                <p>Number of Games Played: {teamDetails.NumberOfGamesPlayed}</p>
+              </div>
             </div>
-          </div>
-          <div className="card mb-4">
-            <div className="card-header">
-              <h4>Games Played</h4>
+            <div className="card mb-4">
+              <div className="card-header">
+                <h4>Total Score</h4>
+              </div>
+              <div className="card-body">
+                <p>Total Score: {teamDetails.TotalScore}</p>
+              </div>
             </div>
-            <div className="card-body">
-              <p>Number of Games Played: {teamDetails.NumberOfGamesPlayed}</p>
-            </div>
-          </div>
-          <div className="card mb-4">
-            <div className="card-header">
-              <h4>Total Score</h4>
-            </div>
-            <div className="card-body">
-              <p>Total Score: {teamDetails.TotalScore}</p>
-            </div>
-          </div>
-        </>
-      ) : (
-        <p>Loading team details...</p>
-      )}
-    </div>
+          </>
+        ) : (
+          <p>Loading team details...</p>
+        )}
+      </div>
     </div>
   );
 };
